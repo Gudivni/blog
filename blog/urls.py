19f3fa30011938cef60963c7e2506e4717421df7
Gudivni <tamerlan.almazbekov@gmail.com>
2023-02-09 21:35:07 +0600

    add

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from posts.views import hello_view, youtube_view, google_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_view),
+    path('youtube/', youtube_view),
+    path('google/', google_view)
 ]
